f8bb79933a631ffb257ceaa8e576ac8990e3b2c2
Mukavuuspalvelu <info@mukavuuspalvelu.fi>
2025-06-11 18:18:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Mukavuuspalvelu Chat-palvelin on päällä.");
-});
+// Показываем статические файлы из папки public
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
 
