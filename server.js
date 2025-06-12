71a490eb2023117c7ae4642414b59b006eb098f4
Mukavuuspalvelu <info@mukavuuspalvelu.fi>
2025-06-12 13:06:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const webhookRoutes = require("./routes/webhook");
-const chatRoute = require("./routes/chat");
-
+const chatRoute = require("./routes/chat"); // 
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,11 +11,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", webhookRoutes);
-app.use("/", chatRoute);
+app.use("/", chatRoute); //
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
 
 
 
