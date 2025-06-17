da7fbd0a49d4874803632e44844a896270480961
Mukavuuspalvelu <info@mukavuuspalvelu.fi>
2025-06-17 13:04:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 
 const app = express();
 const webhookRoutes = require("./routes/webhook");
 
-app.use(cors());
 app.use(bodyParser.json());
-app.use("/webhook", webhookRoutes); // 
+app.use("/", webhookRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(Server is running on port ${PORT});
 });
 
 
