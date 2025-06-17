97603c68b638d201a0c84c80ad4efb676e83d1f7
Mukavuuspalvelu <info@mukavuuspalvelu.fi>
2025-06-17 21:53:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use("/chat", chatRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("✅ Server is running on port " + PORT);
+  console.log(Server is running on port " + PORT);
 });
 
 
